dbf953cd755869f3cb5b3f3beee39731d2879e42
Pavan K L <pavankl275@gmail.com>
2026-06-28 21:36:01 +0530

    remove element

---
+++ Arrays/Remove.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        write_index = 0
+        for read_index in range(len(nums)):
+
+            if nums[read_index] != val:
+
+                nums[write_index] = nums[read_index]
+                write_index += 1
+                
+        return write_index
