34233fe15637a2990f27e92166c5dc93de0a5fa7
Pavan K L <pavankl275@gmail.com>
2026-07-03 09:12:54 +0530

    self crossing problem

---
+++ Arrays/Selfcrossing.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isSelfCrossing(self, distance):
+        n = len(distance)
+        if n <= 3:
+            return False
+            
+        for i in range(3, n):
+            if distance[i] >= distance[i-2] and distance[i-1] <= distance[i-3]:
+                return True
+                
+            if i >= 4 and distance[i-1] == distance[i-3] and (distance[i] + distance[i-4] >= distance[i-2]):
+                return True
+                
+            if i >= 5 and (distance[i-2] >= distance[i-4] and 
+                           distance[i-1] <= distance[i-3] and 
+                           distance[i] + distance[i-4] >= distance[i-2] and 
+                           distance[i-1] + distance[i-5] >= distance[i-3]):
+                return True
+                
+        return False
