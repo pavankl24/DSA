880d5c59c22fa98a527f168675340843eb4d1503
Pavan K L <pavankl275@gmail.com>
2026-07-12 19:25:31 +0530

    solved Continuous Subarray Sum problem

---
+++ Arrays/continuous.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def checkSubarraySum(self, nums, k):
+        remainder_map = {0: -1}
+        current_sum = 0
+        
+        for i, num in enumerate(nums):
+            current_sum += num
+            remainder = current_sum % k
+            
+            if remainder in remainder_map:
+                if i - remainder_map[remainder] >= 2:
+                    return True
+            else:
+                remainder_map[remainder] = i
+                
+        return False
