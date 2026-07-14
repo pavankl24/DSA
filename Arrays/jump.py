fcf85165b912430a46ee06f8aecfd2586fdfe9c7
Pavan K L <pavankl275@gmail.com>
2026-07-14 11:30:29 +0530

    solved jump game problem

---
+++ Arrays/jump.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def canJump(self, nums):
+        max_reachable = 0
+        
+        for i, jump in enumerate(nums):
+            if i > max_reachable:
+                return False
+            max_reachable = max(max_reachable, i + jump)
+            
+        return True
