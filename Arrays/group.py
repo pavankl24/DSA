d7b8a62bd0cd350a63750f5be4b0a487da69f67d
Pavan K L <pavankl275@gmail.com>
2026-07-16 18:54:51 +0530

    solved group anagrams problem

---
+++ Arrays/group.py
@@ -0,0 +1,9 @@
+from collections import defaultdict
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        anagrams = defaultdict(list)
+        for s in strs:
+            key = "".join(sorted(s))
+            anagrams[key].append(s)
+        return list(anagrams.values())
