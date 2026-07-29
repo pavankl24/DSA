90028bc516c0b86ef5d50971729e452e896ff121
Pavan K L <pavankl275@gmail.com>
2026-07-29 21:39:11 +0530

    solved zero array transformation problem

---
+++ Arrays/zeroarray.py
@@ -0,0 +1,19 @@
+from heapq import heappush, heappop
+
+class Solution(object):
+    def maxRemoval(self, nums, queries):
+        queries.sort()
+        pq = []
+        d = [0] * (len(nums) + 1)
+        s = j = 0
+        for i, x in enumerate(nums):
+            s += d[i]
+            while j < len(queries) and queries[j][0] <= i:
+                heappush(pq, -queries[j][1])
+                j += 1
+            while s < x and pq and -pq[0] >= i:
+                s += 1
+                d[-heappop(pq) + 1] -= 1
+            if s < x:
+                return -1
+        return len(pq)
