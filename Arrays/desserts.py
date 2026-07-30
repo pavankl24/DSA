5dbd843a67045419283c6701e153dc03f47addbe
Pavan K L <pavankl275@gmail.com>
2026-07-30 07:30:35 +0530

    solved Closest Dessert Cost problem

---
+++ Arrays/desserts.py
@@ -0,0 +1,21 @@
+class Solution(object):
+
+    def closestCost(self, baseCosts, toppingCosts, target):
+        self.closest = float("inf")
+
+        def dfs(idx, current_cost):
+            if abs(current_cost - target) < abs(self.closest - target):
+                self.closest = current_cost
+            elif abs(current_cost - target) == abs(self.closest - target):
+                self.closest = min(self.closest, current_cost)
+
+            if idx == len(toppingCosts) or current_cost >= target:
+                return
+
+            for count in range(3):
+                dfs(idx + 1, current_cost + toppingCosts[idx] * count)
+
+        for base in baseCosts:
+            dfs(0, base)
+
+        return self.closest
