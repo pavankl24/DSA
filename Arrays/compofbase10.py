786062e011c221cf755a7357f469b12abd1cbaec
Pavan K L <pavankl275@gmail.com>
2026-08-06 22:22:13 +0530

    solved complement for base 10 problem

---
+++ Arrays/compofbase10.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def bitwiseComplement(self, n):
+        if n == 0:
+            return 1
+        
+        mask = 1
+        while mask < n:
+            mask = (mask << 1) | 1
+            
+        return n ^ mask
