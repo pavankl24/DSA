e5c4dc07964d7d3c3baf60de2c26cb28f6055bf2
Pavan K L <pavankl275@gmail.com>
2026-08-08 20:19:04 +0530

    solved Find the Lexicographically Smallest Valid Sequence problem

---
+++ Arrays/lexicograpy.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def validSequence(self, word1, word2):
+        n, m = len(word1), len(word2)
+        
+        last = [-1] * m
+        j = m - 1
+        for i in range(n - 1, -1, -1):
+            if j >= 0 and word1[i] == word2[j]:
+                last[j] = i
+                j -= 1
+                
+        ans = []
+        j = 0
+        changed = False
+        
+        for i in range(n):
+            if j == m:
+                break
+                
+            if word1[i] == word2[j]:
+                ans.append(i)
+                j += 1
+            else:
+                if not changed:
+                    if j == m - 1 or (j + 1 < m and last[j + 1] > i):
+                        ans.append(i)
+                        j += 1
+                        changed = True
+                else:
+                    if j < m and last[j] > i:
+                        pass
+        
+        return ans if len(ans) == m else []
