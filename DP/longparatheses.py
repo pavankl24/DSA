2d7f6a19cbb6df3241366e3e0aa9519f7a3733d1
Pavan K L <pavankl275@gmail.com>
2026-08-11 22:37:05 +0530

    solved Longest Valid Parentheses problem

---
+++ DP/longparatheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestValidParentheses(self, s):
+        max_len = 0
+        stack = [-1]
+        
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    max_len = max(max_len, i - stack[-1])
+                    
+        return max_len
