6d95f63147b6345ecb9d1a3c0541c77ce229e275
Pavan K L <pavankl275@gmail.com>
2026-08-12 20:49:44 +0530

    solved decode ways problem

---
+++ DP/decodeways.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def numDecodings(self, s):
+        if not s or s[0] == '0':
+            return 0
+            
+        n = len(s)
+        prev2 = 1
+        prev1 = 1
+        
+        for i in range(1, n):
+            current = 0
+            
+            if s[i] != '0':
+                current += prev1
+                
+            two_digit = int(s[i-1:i+1])
+            if 10 <= two_digit <= 26:
+                current += prev2
+                
+            if current == 0:
+                return 0
+                
+            prev2 = prev1
+            prev1 = current
+            
+        return prev1
