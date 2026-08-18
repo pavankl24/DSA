b834cccb619f09f7d00768ddd55cd0f1ae329d57
Pavan K L <pavankl275@gmail.com>
2026-08-18 21:33:53 +0530

    solved target sum problem

---
+++ DP/targetsum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findTargetSumWays(self, nums,target):
+        total_sum = sum(nums)
+        
+        if (total_sum + target) % 2 != 0 or abs(target) > total_sum:
+            return 0
+            
+        subset_sum = (total_sum + target) // 2
+        
+        dp = [0] * (subset_sum + 1)
+        dp[0] = 1
+        
+        for num in nums:
+            for j in range(subset_sum, num - 1, -1):
+                dp[j] += dp[j - num]
+                
+        return dp[subset_sum]
