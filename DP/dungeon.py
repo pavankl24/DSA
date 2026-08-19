2147b8966be22270f080f26dfb5d05d156f60474
Pavan K L <pavankl275@gmail.com>
2026-08-19 23:31:28 +0530

    solved dungeon game problem

---
+++ DP/dungeon.py
@@ -0,0 +1,14 @@
+class Solution:
+    def calculateMinimumHP(self, dungeon):
+        m, n = len(dungeon), len(dungeon[0])
+        dp = [[float('inf')] * (n + 1) for _ in range(m + 1)]
+        
+        dp[m][n - 1] = 1
+        dp[m - 1][n] = 1
+        
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                min_health_next = min(dp[i + 1][j], dp[i][j + 1])
+                dp[i][j] = max(1, min_health_next - dungeon[i][j])
+                
+        return dp[0][0]
